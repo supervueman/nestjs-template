971cdc2035661758f870dc1f3019fdb56349e010
supervueman <supervueman@gmail.com>
2020-01-11 17:04:13 +0400

    Add db postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import 'dotenv/config';
+import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`Server start on http://localhost:${port}`);
 }
 bootstrap();
