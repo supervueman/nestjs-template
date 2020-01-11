616f3fa94da12efa12dfb5c983d1386f519889f0
supervueman <supervueman@gmail.com>
2020-01-11 16:08:15 +0400

    Test get method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get()
+  getUser(): string {
+
+    return '<h1>Rinat</h1>';
+  }
+  @Post()
+  createUser(): void {
+    // tslint:disable-next-line: no-console
+    console.log('Created user');
+  }
+}
