546423018d7b2a0fd8e08e356e1d0b9185ba6ec4
supervueman <supervueman@gmail.com>
2020-01-11 15:40:18 +0400

    Add modules user auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule],
 })
 export class AppModule { }
