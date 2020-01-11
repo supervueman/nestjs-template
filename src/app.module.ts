9eaa7e1f633483e411390078f6b5102a97a737ab
supervueman <supervueman@gmail.com>
2020-01-11 23:48:14 +0400

    Add crud operation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { IdeaModule } from './idea/idea.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule, IdeaModule],
 })
 export class AppModule { }
