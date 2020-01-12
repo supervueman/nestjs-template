1c870b3fc5792f8b99f9c660c881e04a00e606ab
supervueman <supervueman@gmail.com>
2020-01-12 17:03:22 +0400

    Add error logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,18 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { IdeaModule } from './idea/idea.module';
+import { SharedModule } from './shared/shared.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpErrorFilter } from './shared/http-error.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule, IdeaModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule, IdeaModule, SharedModule],
+  providers: [{
+    provide: APP_FILTER,
+    useClass: HttpErrorFilter,
+  }, {
+    provide: APP_INTERCEPTOR,
+    useClass: LoggingInterceptor,
+  }],
 })
 export class AppModule { }
