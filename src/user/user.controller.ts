d6d010db4be1861940031792f821226186c1c358
supervueman <supervueman@gmail.com>
2020-01-13 22:05:40 +0400

    clear

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,12 @@
 import { Controller, Get, Post, Body, UsePipes, ValidationPipe, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDTO } from './user.dto';
-import { AuthGuard } from 'src/shared/auth.guard';
-import { User } from './user.decorator';
 
 @Controller()
 export class UserController {
   constructor(private userService: UserService) { }
   @Get('api/users')
-  @UseGuards(new AuthGuard())
-  showAllUsers(@User() user) {
-    // tslint:disable-next-line: no-console
-    console.log(user);
+  showAllUsers() {
     return this.userService.showAll();
   }
 
